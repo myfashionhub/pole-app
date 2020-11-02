5f4cff72aaa2a4aa28b02eb089466bf28a82f940
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2020-11-01 22:28:24 -0500

    Set up base route & React component

---
+++ app/javascript/components/Index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Index extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>It's me</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Index
